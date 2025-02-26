d1fe5074e4807cbc45d4c84b5f904ea42b6ed870
Laura Esmoris <lesmoris0web@gmail.com>
2025-02-26 18:16:54 -0500

    routes done

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.get("/game", playRoutes)
 
 
 app.listen(PORT, () => {
---
+++ src/routes/routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+
+const router= Router()
+
+router.get("/play",)
+
+export default router
